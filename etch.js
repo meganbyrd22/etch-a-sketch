500f5c790489cbe0663a2951c8e2056757b03e93
Megan Byrd <meganbyrd22@gmail.com>
2022-11-29 10:13:10 -0600

    Create html element in js Create function to create grid in js

---
+++ etch.js
@@ -0,0 +1,9 @@
+//add html elements
+let gridContainer = document.getElementById("gridContainer");
+
+//function to create divs
+function createDivs(){
+   let divs = document.createElement("div"){
+    gridContainer.appendChild("divs")
+   }
+}
