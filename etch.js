3eacc579d45dbec273d7970eaccf7140bef3fd73
Megan Byrd <meganbyrd22@gmail.com>
2022-11-29 14:59:04 -0600

    Add function to create grid

--- etch.js
+++ etch.js
@@ -1,6 +1,18 @@
 //add html elements
 const gridContainer = document.getElementById("gridContainer")
 
-//create divs
-let cell = document.createElement("div");
-gridContainer.appendChild(cell);
+//create cell element
+//let cell = document.createElement("div");
+//gridContainer.appendChild(cell);
+
+//funtion to create grid
+function createGrid(x){
+    for (let rows = 0; rows <=x; rows++)
+    {
+        for (let columns = 0; columns < x; columns++ ){
+
+        let cell = document.createElement("div"); 
+        gridContainer.appendChild(cell);
+    }
+    createGrid(16);
+}}
