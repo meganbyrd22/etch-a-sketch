4937980676f3515c66787e8539a698a610b17e23
Megan Byrd <meganbyrd22@gmail.com>
2022-12-06 10:09:11 -0600

    Add createNewGrid function with numSquares as parameter

--- etch.js
+++ etch.js
@@ -5,12 +5,12 @@ let numSquares = " " //add div in html?
 
 //button function for prompt 
 newGridButton.addEventListener('click', function(){let numSquares = prompt("Please enter the number of squares you'd like for each side of your grid")
-    if (numSquares >= 100){
+    if (parseInt(numSquares) >= 100){
         return "Please enter a number that is less than 100"}
        else {createGrid(parseInt(numSquares))}
     })
 
-//funtion to create grid
+//funtion to create original grid
 function createGrid(){
     //create grid
     for (let rows = 0; rows <16; rows++)
@@ -34,14 +34,31 @@ function createGrid(){
 
 createGrid(16);
 
+
 //function to change color 
 function changeColor(e){
     e.target.style.backgroundColor = 'black';
 }
 
-
-
-//createNewGrid(numSquares); Do I need this if it's called in the else statement?
+function createNewGrid(){
+    for (let rows = (parseInt(numSquares)); rows <100; rows++)
+    {
+    for (let columns = (parseInt(numSquares)); columns < 16; columns++ ){
+    
+     //create cell element    
+    let cell = document.createElement("div");
+        
+    //name cell element       
+    cell.setAttribute("id", "cell"); 
+    
+    
+    cell.addEventListener('mouseover', changeColor)
+       
+    //append to DOM
+    gridContainer.appendChild(cell)
+    }  
+}
+}
 
 //function to create new grid from user response
 //function createNewGrid(){
